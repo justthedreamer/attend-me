17f5d64d43aeb85eeac5388be78eaaf10311eed2
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:56:43 +0100

    update routes.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,13 @@
-import {Role} from "@/model/app/Roles";
-import ErrorView from "@/views/ErrorView.vue";
-import HomeView from "@/views/HomeView.vue";
-import LecturerView from "@/views/LecturerView.vue";
-import StudentView from "../views/StudentDesktop.vue";
-import type {RouteRecord, RouteRecordRaw} from "vue-router";
+import {Role} from "../model/Roles.ts";
+import HomeView from "../views/Common/HomeView.vue";
+import type {RouteRecordRaw} from "vue-router";
+import StudentAttendanceView from "../views/Student/StudentAttendanceView.vue";
+import LecturerCourseDetailsView from "../views/Lecturer/LecturerCourseDetailsView.vue";
+import LecturerDesktopView from "../views/Lecturer/LecturerDesktopView.vue";
+import StudentDesktopView from "../views/Student/StudentDesktopView.vue";
+import StudentCourseDetailsView from "../views/Student/StudentCourseDetailsView.vue";
+import StudentDeviceRegistrationView from "../views/Student/StudentRegisterDeviceView.vue";
+import LecturerAttendanceScannerView from "../views/Lecturer/LecturerAttendanceScannerView.vue";
 
 export default class Routes {
 
@@ -13,46 +17,64 @@ export default class Routes {
         component: HomeView,
     };
 
-    public static readonly StudentRoute: RouteRecordRaw = {
-        path: "/student",
-        name: "student",
-        component: StudentView,
-        meta: {requireAuth: true, role: Role.STUDENT},
-    };
-
-    public static readonly LecturerRoute: RouteRecordRaw = {
-        path: "/lecturer",
-        name: "lecturer",
-        component: LecturerView,
-        meta: {requireAuth: true, role: Role.LECTURER},
-    };
-
-    public static readonly ErrorRoute: RouteRecordRaw = {
-        path: "/error",
-        name: "error",
-        component: ErrorView,
-    };
-
     public static readonly LecturerDesktop: RouteRecordRaw = {
         path: "/lecturer/desktop",
         name: "lecturer-desktop",
-        component: LecturerView,
+        component: LecturerDesktopView,
         meta: {requireAuth: true, role: Role.LECTURER}
+    }
+
+    public static readonly LecturerCourseDetails: RouteRecordRaw = {
+        path: "/lecturer/course/:courseSessionId",
+        name: "lecturer-course-details",
+        component: LecturerCourseDetailsView,
+        meta: {requireAuth: true, role: Role.LECTURER},
+        props: true,
     }
 
     public static readonly StudentDesktop: RouteRecordRaw = {
         path: "/student/desktop",
         name: "student-desktop",
-        component: StudentView,
+        component: StudentDesktopView,
         meta: {requireAuth: true, role: Role.STUDENT},
+    }
+
+    public static readonly StudentCourseDetails: RouteRecordRaw = {
+        path: "/student/course/:courseGroupId",
+        name: "student-course-details",
+        component: StudentCourseDetailsView,
+        props: true,
+    }
+
+    public static readonly StudentDeviceRegistration: RouteRecordRaw = {
+        path: "/student/device/register/:token",
+        name: "student-device-registration",
+        component: StudentDeviceRegistrationView,
+        props: true,
+    }
+
+    public static readonly StudentAttendance: RouteRecordRaw = {
+        path: "/student/attendance",
+        name: "student-attendance",
+        component: StudentAttendanceView,
+        meta: {requireAuth: true, role: Role.STUDENT}
+    }
+
+    public static readonly LecturerAttendanceScanner: RouteRecordRaw = {
+        path: "/lecturer/attendance/scanner/:token",
+        name: "lecturer-attendance-scanner",
+        component: LecturerAttendanceScannerView,
+        props: true,
     }
 
     public static readonly All: RouteRecordRaw[] = [
         this.HomeRoute,
-        this.StudentRoute,
-        this.LecturerRoute,
-        this.ErrorRoute,
-        this.LecturerDesktop,
         this.StudentDesktop,
+        this.LecturerDesktop,
+        this.LecturerCourseDetails,
+        this.StudentCourseDetails,
+        this.StudentAttendance,
+        this.StudentDeviceRegistration,
+        this.LecturerAttendanceScanner,
     ];
 }
