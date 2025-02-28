7bb766abdbfba8b0071fdb0d01d755a475d55120
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:44:26 +0100

    add attendance state component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Course/AttendanceState.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+import Routes from "../../router/routes.ts";
+
+const router = useRouter()
+
+const props = defineProps({
+  attendance: Boolean,
+  courseSessionId: Number,
+})
+
+const navigateToAttendanceView = async () => {
+  await router.push({name: Routes.StudentAttendance.name})
+}
+
+</script>
+
+<template>
+  <button class="btn-primary"
+          v-if="!props.attendance"
+          @click="navigateToAttendanceView">Attend Me
+  </button>
+  <p v-if="props.attendance">Attended &#10004;</p>
+</template>
