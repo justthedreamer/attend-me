a36f470e322eba50911d7bed52c5d0aa76faf0eb
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:48:26 +0100

    add lecturer course cart component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Lecturer/LecturerCourseCart.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+import Routes from "../../router/routes.ts";
+
+const router = useRouter()
+
+const props = defineProps({
+  courseSessionId: Number,
+  courseGroupId: Number,
+  courseGroupName: String,
+  courseName: String,
+  courseDate: String,
+  sessionTimeStart: String,
+  sessionTimeEnd: String,
+  locationName: String,
+})
+
+const navigateToCourseDetails = async (courseSessionId: number) => {
+  await router.push({
+    name: Routes.LecturerCourseDetails.name,
+    params: {courseSessionId: courseSessionId}
+  })
+}
+
+</script>
+
+<template>
+  <div class="rounded-cart bg-gradient-polar-blue-to-green">
+    <h2>{{ props.courseName }}</h2>
+    <p>Group: {{ props.courseGroupName }}</p>
+    <p>Date: {{ props.courseDate }}</p>
+    <p>Time: {{ props.sessionTimeStart }}-{{ props.sessionTimeEnd }}</p>
+    <p>Location: {{ props.locationName }}</p>
+    <button class="btn-secondary" @click="navigateToCourseDetails(props.courseSessionId)">Details</button>
+  </div>
+</template>
