ce10b97b8a839db7aa2c854a1239c4c23b0bf079
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:56:07 +0100

    implement student course details view.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/views/Student/StudentCourseDetailsView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import StudentCourseDetailsList from "../../components/Student/StudentCourseDetailsList.vue";
+import {onMounted} from "vue";
+import {Backend} from "../../main.ts";
+
+onMounted(async () => {
+  await Backend.restoreTokens()
+})
+</script>
+
+<template>
+  <h1>Course details</h1>
+  <StudentCourseDetailsList></StudentCourseDetailsList>
+</template>
+
+<style scoped>
+h1{
+  text-align: center;
+}
+</style>
