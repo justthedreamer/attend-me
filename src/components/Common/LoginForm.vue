1eb15f7cb67ffea0afd7158f52413b3aaae5897d
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:43:43 +0100

    update login form.l
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/components/Common/LoginForm.vue
+++ src/components/Common/LoginForm.vue
@@ -1,73 +1,68 @@
 <script setup lang="ts">
-import {inject, ref} from "vue";
-import {Backend} from "../main.ts";
-import {Auth} from "../helpers/Auth.ts";
+import {ref} from "vue";
 import {useRouter} from "vue-router";
-import Routes from "../router/routes.ts";
-import {Role} from "../model/app/Roles.ts";
-import type {TokenResult} from "../backend/AttendMeBackendClientBase.ts";
-import {ServiceCollection} from "../DI.ts";
-import type {ISessionStorage} from "../auth/ISessionStorage.ts";
-
-interface LoginForm {
-  login: string;
-  password: string;
-}
-
-const formData = ref<LoginForm>({
-  login: "",
-  password: "",
-})
-const usernameErrorMessage = ref<string>("")
-const passwordErrorMessage = ref<string>("")
+import {Backend} from "../../main.ts";
+import useEventBus from "../../events/EventBus.ts";
+import {ErrorMessage, SuccessMessage} from "../../events/MessageEvents.ts";
+import {redirectToDesktopAsync} from "../../helpers/RediretionHelper.ts";
+import {SessionStorage} from "../../auth/SessionStorage.ts";
 
 const router = useRouter();
-const sessionStorage = inject<ISessionStorage>(ServiceCollection.SESSION_STORAGE.key)
+const {emit} = useEventBus()
 
-const handleLoginFormSubmit = async (): Promise<void> => {
+const login = ref<string>("");
+const password = ref<string>("");
+
+const handleLogIn = async (): Promise<void> => {
   try {
-    // temporary use credentials from configuration. todo
-    const loginResult: TokenResult = await Backend.userLogin(Auth.lecturer_login, Auth.lecturer_password)
-
-    if (!loginResult) {
-      throw new Error("Login failed.") //todo
-    }
-
-    const role = sessionStorage.getRole()
-
-    switch (role) {
-      case Role.STUDENT: {
-        await router.push(Routes.StudentDesktop.path)
-        break;
-      }
-      case Role.LECTURER: {
-        await router.push(Routes.LecturerDesktop.path)
-        break;
-      }
-      default: {
-        throw new Error("Role is not supported." + role)
-      }
-    }
+    await Backend.userLogin(login.value, password.value);
+    const role = SessionStorage.getRole()
+    await redirectToDesktopAsync(router, role)
+    emit(SuccessMessage, "Login successful.")
   } catch (error) {
-    throw error;
+    emit(ErrorMessage, error.message)
   }
 }
 </script>
 
 <template>
-  <div class="position-fixed top-50 start-50 translate-middle bg-light p-4 border rounded shadow "
-       style="max-width: 400px; width: 100%;">
-    <h1>AttendMe</h1>
-    <form class="form" @submit.prevent="handleLoginFormSubmit">
-      <label for=" login" class="form-label">Login</label>
-      <input id="login" v-model="formData.login" type="text" placeholder="login" class="form-control" value="stu14705"/>
-      <p class="form-text">{{ usernameErrorMessage.message }}</p>
-      <label for="password" class="form-label">Password</label>
-      <input id="password" v-model="formData.password" type="password" placeholder="password" class="form-control"
-             value="14705"/>
-      <p class="form-text">{{ passwordErrorMessage.message }}</p>
-      <button type="submit" class="btn btn-primary form-control mt-2" @click="Login">Login</button>
+  <div id="login-wrapper">
+    <form id="login-form"
+          class="form form-column light-form form-row-column"
+          @submit.prevent="handleLogIn">
+      <header>AttendMe</header>
+
+      <div class="form-row">
+        <label for="login-input">Login</label>
+        <input id="login-input"
+               type="text"
+               placeholder="Enter your login"
+               required
+               v-model="login">
+      </div>
+
+      <div class="form-row">
+        <label for="password-input">Password</label>
+        <input id="password-input"
+               type="password"
+               placeholder="Enter your password"
+               required
+               v-model="password">
+      </div>
+
+      <button class="btn-primary" type="submit">Sign In</button>
     </form>
   </div>
 </template>
+
+<style scoped>
+#login-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
 
