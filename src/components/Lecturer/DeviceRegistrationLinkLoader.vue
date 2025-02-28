2c3fd2ea219dd8d56119c5bfce18772a7a1c586d
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:44:54 +0100

    add device registration link loader component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Lecturer/DeviceRegistrationLinkLoader.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {Backend} from "../../main.ts";
+import {useRouter} from "vue-router";
+import Routes from "../../router/routes.ts";
+import useEventBus from "../../events/EventBus.ts";
+import {ErrorMessage, SuccessMessage} from "../../events/MessageEvents.ts";
+
+const props = defineProps({
+  userId: Number
+})
+
+const router = useRouter()
+const {emit} = useEventBus()
+
+const copyLink = async () => {
+  try {
+    const tokenResult = await Backend.userDeviceRegisterTokenGet(props.userId)
+
+    const relativePath = router.resolve({
+      name: Routes.StudentDeviceRegistration.name,
+      params: {token: tokenResult.token}
+    }).href;
+
+    const fullUrl = `${window.location.origin}${relativePath}`
+
+    await navigator.clipboard.writeText(fullUrl);
+    emit(SuccessMessage, "Link copied.")
+
+  } catch (error) {
+    emit(ErrorMessage, "Cannot get or copy link.", error.message)
+  }
+}
+
+</script>
+
+<template>
+  <button class="btn-primary" @click="copyLink">Get link</button>
+</template>
+
+<style scoped>
+button {
+  font-size: 1rem;
+  max-width: 100px;
+}
+</style>
