5d1976f9c40a594aaab27b594265696db70b9410
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:57:00 +0100

    update app and min.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,47 @@
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router'
+import {RouterView, useRouter} from 'vue-router'
+import UINotificator from "./components/Common/UINotificator.vue";
+import {onMounted, onUnmounted} from "vue";
+import useEventBus from "./events/EventBus.ts";
+import {checkIsAccessTokenExpired} from "./helpers/SessionHelper.ts";
+import {ErrorMessage} from "./events/MessageEvents.ts";
+import Routes from "./router/routes.ts";
+
+const router = useRouter()
+const {emit} = useEventBus()
+let tokenExpiredCheckIntervalId;
+
+
+const tokenExpirationAsyncHook = async () => {
+  if (checkIsAccessTokenExpired()) {
+    emit(ErrorMessage, "Session expired, you must sign in.")
+    await router.push({name: Routes.HomeRoute.name})
+  }
+}
+
+onMounted(async () => {
+  tokenExpiredCheckIntervalId = setInterval(tokenExpirationAsyncHook, 60_000)
+})
+
+onUnmounted(() => {
+  clearInterval(tokenExpiredCheckIntervalId)
+})
+
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/student">Student</RouterLink>
-        <RouterLink to="/lecturer">Lecturer</RouterLink>
-        <RouterLink to="/error">Error</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView/>
+  <main>
+    <UINotificator/>
+    <RouterView/>
+  </main>
 </template>
-
 <style scoped>
+nav {
+  display: flex;
+  justify-content: space-around;
+}
+
+main {
+  padding: 1rem;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,15 @@ import '@/assets/main.css';
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
-import {ServiceCollection, registerService} from "./DI";
-import {AttendMeBackendClient} from "./client/backend/AttendMebackendClient.ts";
 import backendConfig from "./config/backend.json"
+import {AttendMeBackendClient} from "./backend/AttendMeBackendClient.ts";
+import {VueQrcodeReader} from "vue-qrcode-reader";
 
 const app = createApp(App);
 const backend = new AttendMeBackendClient(backendConfig.base_url)
 
-registerService(app, ServiceCollection.TOKEN_STORAGE);
-registerService(app, ServiceCollection.AUTHENTICATION_SERVICE);
-registerService(app, ServiceCollection.LOGIN_HANDLER);
-
+app.use(VueQrcodeReader)
 app.use(router);
 
 app.mount("#app");
-
 export {backend as Backend}
