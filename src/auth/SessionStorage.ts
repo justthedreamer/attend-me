921b8876b6f1b7da5f20561a38cc69116dc37dde
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:33:56 +0100

    Refactor session storage service.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/auth/SessionStorage.ts
+++ src/auth/SessionStorage.ts
@@ -1,43 +1,23 @@
-import type {ISessionStorage} from "./ISessionStorage.ts";
 import authConfig from "../config/auth.json";
 import {jwtDecode} from "jwt-decode";
-import type {Role} from "../model/app/Roles.ts";
+import type {Role} from "../model/Roles.ts";
 
-export class LocalTokenStorage implements ISessionStorage {
-    get accessToken(): string | null {
-        return sessionStorage.getItem(authConfig.access_token_key);
+export class SessionStorage {
+    static getAccessTokenExpiry(): Date | null {
+        const authData = sessionStorage.getItem(authConfig.access_token_key)
+        const json = JSON.parse(authData);
+        return new Date(json.expires);
     }
 
-    set accessToken(token: string) {
-        sessionStorage.setItem(authConfig.access_token_key, token);
+    static getRole(): Role | null {
+        const authData = sessionStorage.getItem(authConfig.access_token_key)
+        const json = JSON.parse(authData);
+        const token = json.token;
+        const decoded = jwtDecode(token);
+        return decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
     }
 
-
-    get accessTokenExpiry() {
-        return sessionStorage.getItem(authConfig.access_token_expiry_key)
-    }
-
-    set accessTokenExpiry(date: Date) {
-        sessionStorage.setItem(authConfig.access_token_expiry_key, date.toString());
-    };
-
-
-    get deviceToken(): string | null {
-        return sessionStorage.getItem(authConfig.device_token_key);
-    }
-
-    set deviceToken(token: string) {
-        sessionStorage.setItem(authConfig.device_token_key, token);
-    }
-
-
-    get role(): Role {
-        return sessionStorage.getItem(authConfig.role_key)
-    }
-
-    set role(token: string): void {
-        const decodedToken = jwtDecode(token)
-        const role = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
-        sessionStorage.setItem(authConfig.role_key, role);
+    static logout() {
+        sessionStorage.removeItem(authConfig.access_token_key);
     }
 }
