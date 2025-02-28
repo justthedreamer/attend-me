0e0f02b8694851bb322ceb052d1367ac13a73e3e
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-02-28 21:43:49 +0100

    add navigation component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Common/Navigation.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+import Routes from "../../router/routes.ts";
+import {SessionStorage} from "../../auth/SessionStorage.ts";
+
+const router = useRouter()
+
+const handleLogout = async () => {
+  SessionStorage.logout()
+  await router.push({name: Routes.HomeRoute.name})
+}
+</script>
+
+<template>
+  <nav>
+    <div id="logo">AttendMe</div>
+    <button id="logout"
+            @click="handleLogout">&#10149;
+    </button>
+  </nav>
+  <hr>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+#logo, #logout {
+  font-size: 3rem;
+  color: var(--color-steel-blue);
+}
+
+#logout {
+  background-color: transparent;
+  border: none;
+}
+
+</style>
